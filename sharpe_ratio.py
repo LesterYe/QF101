b3973ea8077dbb34c34ecc3250af5700b0ed4a71
LesterYe <48324429+LesterYe@users.noreply.github.com>
2024-06-08 19:11:41 +0400

    added main() function to prevent certain function from running when importing functions from sharpe_ratio.py

--- sharpe_ratio.py
+++ sharpe_ratio.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import numpy as np
 
+def main():
+  print(f'Size of dataset = {df.size}')
+  print(f'Number of observations of dailyRet = {num_obs - 1}')
+  print(f'Number of observations of excessRet = {num_obs - 1}')
+  print(f'Sharpe Ratio = {sharpe_ratio}')
+
 def create_daily_returns(adjCls):
   # dailyRet = adjCls.pct_change(periods=1)
 
@@ -33,20 +39,19 @@ def calculate_sharpe_ratio(excessRet, num_obs):
 df = pd.read_csv('IGE.csv', header=0, parse_dates=[0], index_col=0)
 # print(df.iat[0,0]) # [row,col]
 # print(df.loc['2023-05']) # filter month / year
-print(f'Size of dataset = {df.size}')
 num_obs = df.shape[0]
 
 #==================== Create daily returns dataset ====================#
 adjCls = df[['Adj Close']] # or use .to_frame()
 dailyRet = create_daily_returns(adjCls)
-print(f'Number of observations of dailyRet = {num_obs - 1}')
 # print(type(dailyRet))
 
 #==================== Create excess returns dataset ====================#
 rfr = 0.04
 excessRet = create_excess_returns(dailyRet, rfr, num_obs)
-print(f'Number of observations of excessRet = {num_obs - 1}')
 
 #==================== Calculate sharpe ratio ====================#
 sharpe_ratio = calculate_sharpe_ratio(excessRet, num_obs)
-print(f'Sharpe Ratio = {sharpe_ratio}')
+
+if __name__ == '__main__':
+  main()
