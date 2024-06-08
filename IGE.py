229569554624800111ffbd05d74ceb4e35f9736a
LesterYe <48324429+LesterYe@users.noreply.github.com>
2024-06-08 17:16:57 +0400

    update num_obs variable

--- IGE.py
+++ IGE.py
@@ -2,16 +2,15 @@ import pandas as pd
 import numpy as np
 
 def create_daily_returns(adjCls):
-  num_obs = adjCls.size
-  dailyRet = adjCls.pct_change(periods=1)
+  # dailyRet = adjCls.pct_change(periods=1)
 
   # alternative method
-  # firstDiff = adjCls.diff(periods=1)
-  # dailyRet2 = firstDiff / adjCls.shift(periods=1)
+  firstDiff = adjCls.diff(periods=1)
+  dailyRet = firstDiff / adjCls.shift(periods=1)
   # compare_df = dailyRet2 - dailyRet
   # print(compare_df.sum())
 
-  return dailyRet, num_obs
+  return dailyRet
 
 def create_excess_returns(dailyRet, rfr, num_obs):
   excessRet = dailyRet - rfr/num_obs
@@ -35,10 +34,11 @@ df = pd.read_csv('IGE.csv', header=0, parse_dates=[0], index_col=0)
 # print(df.iat[0,0]) # [row,col]
 # print(df.loc['2023-05']) # filter month / year
 print(f'Size of dataset = {df.size}')
+num_obs = df.shape[0]
 
 #==================== Create daily returns dataset ====================#
 adjCls = df[['Adj Close']] # or use .to_frame()
-dailyRet, num_obs = create_daily_returns(adjCls)
+dailyRet = create_daily_returns(adjCls)
 print(f'Number of observations of dailyRet = {num_obs - 1}')
 # print(type(dailyRet))
 
@@ -50,4 +50,3 @@ print(f'Number of observations of excessRet = {num_obs - 1}')
 #==================== Calculate sharpe ratio ====================#
 sharpe_ratio = calculate_sharpe_ratio(excessRet, num_obs)
 print(f'Sharpe Ratio = {sharpe_ratio}')
-
